7df9b51bac9915176bd0085831ae49f1a847f63d
Michael Papesca <michael.papesca@onmail.com>
2021-08-11 19:51:11 -0600

    feat: Improve homescreen

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Monargot!</Text>
+      <Text>Welcome to Monargot! I hope we can fix your slang problems!</Text>
       <StatusBar style="auto" />
     </View>
   );
