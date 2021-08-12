752e9c1c16979cb873e641d1d138ab7c39c9723b
Michael Papesca <michael.papesca@onmail.com>
2021-08-11 20:43:21 -0600

    feat: Add NativeBase

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { Box, Center, Heading, NativeBaseProvider, Text } from 'native-base';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Monargot! I hope we can fix your slang problems!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Box p={9}
+          _text={{
+            fontSize: "md",
+            fontWeight: "bold",
+            color: "white",
+          }}
+          shadow={5}
+          rounded="lg"
+        >
+          <Center>
+            <Heading>Welcome to Monargot!</Heading>
+            <Text>I hope we can fix your slang problems!</Text>
+          </Center>
+        </Box>
+      </Center>
+    </NativeBaseProvider >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
