e43ca22c00c73829993855b3aed5dfa400c83e9d
Michael Papesca <michael.papesca@onmail.com>
2021-08-13 23:44:07 -0600

    fix: Make logo show on app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import { Box, Center, Heading, NativeBaseProvider, Text } from 'native-base';
+import { Box, Center, Heading, NativeBaseProvider, Text, Image } from 'native-base';
+// import { Image } from 'react-native';
 import React from 'react';
 import AppBar from './src/components/AppBar';
+import logo from './assets/logo-1.png';
 
 export default function App() {
   return (
@@ -17,6 +19,12 @@ export default function App() {
           rounded="lg"
         >
           <Center>
+            <Image
+              source={logo}
+              alt="Monargot Logo"
+              size={"xl"}
+              rounded="lg"
+            />
             <Heading>Welcome to Monargot!</Heading>
             <Text>I hope we can fix your slang problems!</Text>
           </Center>
